b8b266ce725ff8cfa3a557f2c1486f25c72638c3
lab lab <lab@labinfo.is.escuelaing.edu.co>
2023-09-04 10:31:08 -0500

    Implemented Product Services

--- src/main/java/org/adaschool/api/service/product/ProductsServiceMap.java
+++ src/main/java/org/adaschool/api/service/product/ProductsServiceMap.java
@@ -3,33 +3,40 @@ package org.adaschool.api.service.product;
 import org.adaschool.api.repository.product.Product;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Optional;
 
 @Service
 public class ProductsServiceMap implements ProductsService {
+
+    HashMap<String, Product> products = new HashMap<>();
     @Override
     public Product save(Product product) {
-        return null;
+        products.put(product.getId(), product);
+        return product;
     }
 
     @Override
     public Optional<Product> findById(String id) {
-        return Optional.empty();
+        return Optional.ofNullable(products.get(id));
     }
 
     @Override
     public List<Product> all() {
-        return null;
+        List<Product> allProducts = new ArrayList<>(products.values());
+        return allProducts;
     }
 
     @Override
     public void deleteById(String id) {
-
+        products.remove(id);
     }
 
     @Override
     public Product update(Product product, String productId) {
-        return null;
+        products.put(productId, product);
+        return product;
     }
 }
